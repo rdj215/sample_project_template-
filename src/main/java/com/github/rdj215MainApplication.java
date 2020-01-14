2b8b89b608f504a6c37f30732f45fbe4546ec26c
unknown <ryanjamison26@gmail.com>
2020-01-14 10:13:52 -0500

    update

---
+++ src/main/java/com/github/rdj215MainApplication.java
@@ -0,0 +1,7 @@
+package com.github;
+
+public class rdj215MainApplication {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/test/java/com/github/rdj215Test.java
@@ -0,0 +1,10 @@
+package com.github;
+
+import org.junit.Test;
+
+public class rdj215Test {
+    @Test
+    public void test(){
+
+    }
+}
